566031de6f81d78a01913f776f32706b3fe4bda0
hyeladzirajames <adzirajbalami@gmail.com>
2026-02-06 19:04:56 +0000

    fix databse connection issue3

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,7 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.PGHOST,
+  port: Number(process.env.PGPORT),
+  database: process.env.PGDATABASE,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
   ssl: { rejectUnauthorized: false },
 });
 
