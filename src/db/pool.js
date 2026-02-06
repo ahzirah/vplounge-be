285a8ba2be336abafdac9426f2e496c924fbef66
hyeladzirajames <adzirajbalami@gmail.com>
2026-02-06 18:29:34 +0000

    going live

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,12 +1,25 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  host: process.env.PGHOST,
-  port: Number(process.env.PGPORT),
-  database: process.env.PGDATABASE,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  ssl: { rejectUnauthorized: false },
-});
+function getPoolConfig() {
+  // If Digital Ocean injects DATABASE_URL, use it
+  if (process.env.DATABASE_URL) {
+    return {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    };
+  }
+
+  // Otherwise use PG* vars
+  return {
+    host: process.env.PGHOST,
+    port: Number(process.env.PGPORT || 5432),
+    database: process.env.PGDATABASE,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    ssl: { rejectUnauthorized: false },
+  };
+}
+
+const pool = new Pool(getPoolConfig());
 
 module.exports = { pool };
