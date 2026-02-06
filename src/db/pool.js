74deb0dfe0716255f8ad3f183eaa7610e36747fd
hyeladzirajames <adzirajbalami@gmail.com>
2026-02-06 18:47:48 +0000

    fix databse connection issue

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,25 +1,8 @@
 const { Pool } = require("pg");
 
-function getPoolConfig() {
-  // If Digital Ocean injects DATABASE_URL, use it
-  if (process.env.DATABASE_URL) {
-    return {
-      connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false },
-    };
-  }
-
-  // Otherwise use PG* vars
-  return {
-    host: process.env.PGHOST,
-    port: Number(process.env.PGPORT || 5432),
-    database: process.env.PGDATABASE,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    ssl: { rejectUnauthorized: false },
-  };
-}
-
-const pool = new Pool(getPoolConfig());
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
 
 module.exports = { pool };
