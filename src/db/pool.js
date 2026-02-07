4dedd15c11c417f88450e6ab013303ed196f533f
hyeladzirajames <adzirajbalami@gmail.com>
2026-02-07 21:44:01 +0000

    fixed error on pool

--- src/db/pool.js
+++ src/db/pool.js
@@ -6,7 +6,7 @@ const pool = new Pool({
   database: process.env.PGDATABASE,
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
-  ssl: { rejectUnauthorized: true },
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = { pool };
