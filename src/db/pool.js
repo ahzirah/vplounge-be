511a2532e701dea896ca5733c07e6a17be4073c5
hyeladzirajames <adzirajbalami@gmail.com>
2026-02-07 22:02:40 +0000

    fixed issue of ssl reject unauthorized

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,12 +1,19 @@
 const { Pool } = require("pg");
 
+if (!process.env.PG_CA_CERT) {
+  console.warn("PG_CA_CERT is missing. DB SSL verification may fail.");
+}
+
 const pool = new Pool({
   host: process.env.PGHOST,
   port: Number(process.env.PGPORT),
   database: process.env.PGDATABASE,
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
-  ssl: { rejectUnauthorized: false },
+  ssl: {
+    rejectUnauthorized: true,
+    ca: process.env.PG_CA_CERT,
+  },
 });
 
 module.exports = { pool };
